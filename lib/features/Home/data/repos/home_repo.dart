8684b6dbc8d2adc78bd94b0c25b8c9247b2b9dc1
Tarek BinSelm <binselmt@gmail.com>
2023-12-11 00:57:31 +0200

    create api service (wrapper) & create home repo impl

--- lib/features/Home/data/repos/home_repo.dart
+++ lib/features/Home/data/repos/home_repo.dart
@@ -1,8 +1,8 @@
 import 'package:bookly_app/core/errors/failures.dart';
 import 'package:bookly_app/features/Home/data/models/book_model/book_model.dart';
-import 'package:either_dart/either.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-Future<Either<Failure ,List<BookModel> >>  fetchFeaturedBooks();
-  fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
 }
