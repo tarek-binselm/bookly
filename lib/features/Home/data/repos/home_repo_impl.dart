b43860518683997bcf3b026202ab0d1afb2a37d5
Tarek BinSelm <binselmt@gmail.com>
2023-12-11 02:30:58 +0200

    fetchFeaturedBooks method impl done ( HomeRepo Impl Done )

--- lib/features/Home/data/repos/home_repo_impl.dart
+++ lib/features/Home/data/repos/home_repo_impl.dart
@@ -33,8 +33,24 @@ class HomeRepoImpl implements HomeRepo {
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    // TODO: implement fetchNewestBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async{
+   try {
+      var data = await apiService.get(
+          endPoint:
+              'volumes?q=subject:programming&Filtering=free-ebooks');
+
+      List<BookModel> books = [];
+
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      }
+
+      return left(ServerFailure(e.toString()));
+    }
   }
 }
